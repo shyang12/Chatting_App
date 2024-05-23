46064332d1fbccdd574ac1a4998dafbcc90f86b9
shyang12 <85710913+shyang12@users.noreply.github.com>
2024-05-23 18:52:06 +0900

    채팅 메세지 보내기 구현 / 2024-05-23

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,6 @@
+import 'package:chatting_app/chatting/chat/message.dart';
+import 'package:chatting_app/chatting/chat/new_message.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -49,32 +52,15 @@ class _ChatScreenState extends State<ChatScreen> {
           )
         ],
       ),
-      body: StreamBuilder(
-        stream: FirebaseFirestore.instance.collection('chats/2AxNibHe4xUFI8ioSf8I/message')
-                .snapshots(),
-        builder: (
-            BuildContext context,
-            AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-
-          final docs = snapshot.data!.docs;
-          return ListView.builder(
-            itemCount: docs.length,
-            itemBuilder: (context, index) {
-              return Container(
-                padding: EdgeInsets.all(8.0),
-                child: Text(
-                  docs[index]['text'],
-                  style: TextStyle(fontSize: 20.0),
-                ),
-              );
-            },
-          );
-        },
+      body: Container(
+        child: Column(
+          children: [
+            Expanded(
+                child: Messages(),
+            ),
+            NewMessage(),
+          ],
+        ),
       ),
     );
   }
